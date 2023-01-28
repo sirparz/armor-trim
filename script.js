08e75dfe1dc6ab5b7b4ff5e815243175a49adcf0
Faris <ferishahab11@gmail.com>
2023-01-28 18:54:59 +0300

    Add Message Components & Functions

--- script.js
+++ script.js
@@ -2,6 +2,34 @@
 // For Codecademy Javascript Portfolio
 // Made by: Sir Parz
 
-const trimStyle = ['Sentry', 'Dune', 'Coast', 'Wild', 'Tide', 'Ward', 'Vex', 'Rib', 'Snout', 'Eye', 'Spire'];
-const trimMaterial = ['Iron', 'Copper', 'Gold', 'Lapis Lazuli', 'Diamond', 'Netherite', 'Redstone', 'Amethyst', 'Quartz']
-const armorPiece = ['Helmet', 'Chestplate', 'Leggings', 'Boots']
+const trimStyles = ['Sentry', 'Dune', 'Coast', 'Wild', 'Tide', 'Ward', 'Vex', 'Rib', 'Snout', 'Eye', 'Spire'];
+const templateOrigins = ['Pillager Outpost', 'Desert Pyramid', 'Shipwreck', 'Jungle Temple', 'Ocean Monument', 'Ancient City', 
+  'Woodland Mansion', 'Nether Fortress', 'Bastion Remnant', 'Stronghold', 'End City'];
+
+const trimMaterials = ['Iron', 'Copper', 'Gold', 'Lapis Lazuli', 'Diamond', 'Netherite', 'Redstone', 'Amethyst', 'Quartz'];
+const armorPieces = ['helmet', 'chestplate', 'leggings', 'boots'];
+
+// The Core Random Number Generator
+const RNG = number => Math.floor(Math.random() * number)
+
+const randomTrim = () => {
+  let iChooseYou = RNG(trimStyles.length);
+  return {
+    style: trimStyles[iChooseYou],
+    origin: templateOrigins[iChooseYou],
+  }
+}
+
+const generateSet = () => {
+  let set = {};
+  for (let piece of armorPieces) {
+    set[piece] = {
+      style: randomTrim(),
+      material: trimMaterials[RNG(trimMaterials.length)],
+    }
+  }
+  return set;
+}
+  
+//let nanii = generateSet();
+//console.log(nanii.chestplate)
