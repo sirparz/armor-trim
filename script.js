19c4bc835b1cfd183bf8d228651f585bc3e6e7de
Faris <ferishahab11@gmail.com>
2023-01-28 17:55:19 +0300

    Add Initial Contents

---
+++ script.js
@@ -0,0 +1,7 @@
+// Netherite Armor Random Trim Generator (NARTG)
+// For Codecademy Javascript Portfolio
+// Made by: Sir Parz
+
+const trimStyle = ['Sentry', 'Dune', 'Coast', 'Wild', 'Tide', 'Ward', 'Vex', 'Rib', 'Snout', 'Eye', 'Spire'];
+const trimMaterial = ['Iron', 'Copper', 'Gold', 'Lapis Lazuli', 'Diamond', 'Netherite', 'Redstone', 'Amethyst', 'Quartz']
+const armorPiece = ['Helmet', 'Chestplate', 'Leggings', 'Boots']
