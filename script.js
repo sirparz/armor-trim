350349e3d3ce6a65534121d8d9416fbe75306489
Faris <ferishahab11@gmail.com>
2023-01-28 19:40:51 +0300

    Add Message Generator

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const trimStyles = ['Sentry', 'Dune', 'Coast', 'Wild', 'Tide', 'Ward', 'Vex', 'R
 const templateOrigins = ['Pillager Outpost', 'Desert Pyramid', 'Shipwreck', 'Jungle Temple', 'Ocean Monument', 'Ancient City', 
   'Woodland Mansion', 'Nether Fortress', 'Bastion Remnant', 'Stronghold', 'End City'];
 
-const trimMaterials = ['Iron', 'Copper', 'Gold', 'Lapis Lazuli', 'Diamond', 'Netherite', 'Redstone', 'Amethyst', 'Quartz'];
+const trimMaterials = ['Iron', 'Copper', 'Gold', 'Lapis Lazuli', 'Emerald', 'Diamond', /*'Netherite',*/ 'Redstone', 'Amethyst', 'Quartz'];
 const armorPieces = ['helmet', 'chestplate', 'leggings', 'boots'];
 
 // The Core Random Number Generator
@@ -15,21 +15,41 @@ const RNG = number => Math.floor(Math.random() * number)
 const randomTrim = () => {
   let iChooseYou = RNG(trimStyles.length);
   return {
-    style: trimStyles[iChooseYou],
+    template: trimStyles[iChooseYou],
     origin: templateOrigins[iChooseYou],
   }
 }
 
+// Generate the trimmed armor set randomly
 const generateSet = () => {
   let set = {};
   for (let piece of armorPieces) {
     set[piece] = {
-      style: randomTrim(),
-      material: trimMaterials[RNG(trimMaterials.length)],
+      trimStyle: randomTrim(),
+      trimMaterial: trimMaterials[RNG(trimMaterials.length)],
     }
   }
   return set;
 }
+
+const generateMessage = () => {
+  let armorSet = generateSet();
+  let messageContent = '';
+  //console.log(armorSet);
+  messageContent += '\nGone of the days where everyone look like a cloned with Netherite Armor.\n';
+  messageContent += 'It\'s about time for you to distinguish yourself in Minecraft with the armor trim.\n';
+  messageContent += 'Not sure what trim to choose? We\'re gonna generate the trim for you.\n\n';
+
+  for (let piece of armorPieces) {
+    messageContent += `NETHERITE ${piece.toUpperCase()}: \n`;
+    messageContent += ` - ${armorSet[piece].trimMaterial}-trimmed` + '\n'
+    messageContent += ` - ${armorSet[piece].trimStyle.template} template from the ${armorSet[piece].trimStyle.origin}` + '\n\n'
+  }
+
+  messageContent += 'Good luck on hunting the template from each respective location!\n'
+  messageContent += 'It will take more than one structure to find a smithing template.\n'
+  messageContent += 'Make sure to duplicate the template before the armor trimming operation.\n'
+  return messageContent;
+}
   
-//let nanii = generateSet();
-//console.log(nanii.chestplate)
+console.log(generateMessage())
